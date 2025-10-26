6775bb9a7964946a5e1a80d46b24253c9ad83834
Fabian Kleindienst <fabian.kleindienst@tum.de>
2025-10-26 23:42:17 +0100

    feat: main for testing

---
+++ inc/public/mathlib.hpp
@@ -0,0 +1,23 @@
+/*****************************************************************************************
+ * @file: mathlib.hpp
+ *
+ * @brief: MathLib's main header file.
+ *
+ * @details: This file includes all other public header files.
+ *
+ * @author: fakl
+ * @date: October 2025
+ *
+ ****************************************************************************************/
+
+#pragma once
+
+#include "Complex.hpp"
+#include "DivByZeroError.hpp"
+#include "IndexOutOfRangeError.hpp"
+#include "MathLibError.hpp"
+#include "Vector2D.hpp"
+#include "Vector3D.hpp"
+#include "compare.hpp"
+#include "config.hpp"
+#include "conversion.hpp"
---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*****************************************************************************************
+ * @file: main.cpp
+ *
+ * @brief: This is the main file.
+ *
+ * @details: This file may be used to try some functionality.
+ *
+ * @author: fakl
+ * @date: October 2025
+ *
+ ****************************************************************************************/
+
+#include "mathlib.hpp"
+#include <iostream>
+
+using namespace mathlib;
+
+/*****************************************************************************************
+ *  Main Function
+ ****************************************************************************************/
+int main(void) {
+
+  /* TODO: try it out yourself! */
+
+  Vector2D vec2(1, 2);
+  Vector3D vec3(1, 2, 3);
+
+  std::cout << vec2 << std::endl;
+  std::cout << vec3 << std::endl;
+
+  return 0;
+}
